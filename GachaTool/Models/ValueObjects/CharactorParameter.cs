ad2ab3c5ac4fc30de26ede17d4a189353f78d761
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-07-19 10:42:55 +0900

    キャラクターのパラメータークラスの作成

---
+++ GachaTool/Models/ValueObjects/CharactorParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GachaTool.Models.ValueObjects
+{
+    class CharactorParameter
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public float Probability { get; set; }
+
+        public int Rarity { get; set; }
+
+        public string Image { get; set; }
+    }
+}
